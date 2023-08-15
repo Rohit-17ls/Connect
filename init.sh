ab1ae9f807f1ef9bf41b348cdcfe8a0a0928d2fe
Rohit <rohitd.arsly@gmail.com>
2023-08-15 18:55:45 +0530

    Notifications in browser and timestamped messages

--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-
-echo $(gcc -o connect .src/connect.c && gcc -o client .src/client.c -pthread && gcc -o server .src/server.c -pthread);
+echo $(gcc -o ../connect .src/connect.c && gcc -o client .src/client.c -pthread && gcc -o server .src/server.c -pthread);
+echo "Have a good time using Connect !!\nIn case you face any issue, start an issue on github at https://github.com/Sly-17/Connect/issues";
